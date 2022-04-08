9239b8ef5803b824d06bedffe2db648bd464b528
Ammon Van Engelenhoven <59853126+vanman247@users.noreply.github.com>
2022-04-07 23:44:54 -0600

    asdf

--- C_challenge.py
+++ C_challenge.py
@@ -1,34 +1,24 @@
-# coding_challenge
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
 
-### CSE 250 Coding Challenge
-
-Ammon Van Engelenhoven
-
-### Challenge Summary
-
-_After completing the challenge, describe how you think you did._
-
-#### Challenge 1
-
-The below code reads in the data from one of our projects this semester. In addition, it removes all rows that don't report a month. Use another Pandas method(s) besides .replace() and dropna() to arrive at the same data set.
-
-##### Answer
-
-_Include the image, answer, or table here_
-
-##### Code
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import metrics
 
 def gq_1_example():
     base_url = 'https://github.com/byuidatascience/data4missing/'
     flights_path = 'raw/master/data-raw/flights_missing/flights_missing.json'
     url_flights = base_url + flights_path
 
-    flights = (pd.read_json(url_flights)
+    df = (pd.read_json(url_flights)
         .replace({"month":{"n/a":np.nan}})
         .dropna(subset=['month']))
-    print(flights.info())
+    print(df.info())
+    return df
 
-gq_1_example()
+#gq_1_example()
 
 def gq_1_answer():
     base_url = 'https://github.com/byuidatascience/data4missing/'
@@ -39,20 +29,9 @@ def gq_1_answer():
 
     df = df[df["month"] != "n/a"]
     print(df.info())
-    return
+    return df
 
-gq_1_answer()
-
-#### Challenge 2
-
-Two of the minutes columns have missing values. Identify those two columns and replace the missing values with the respective median of those columns. After you have fixed the missing values in those two columns report the standard deviation of each column.
-
-##### Answer
-
-This was rather simple to solve after pulling the Data in from Question 1 I noticed that 2 columns were not the same as the rest for the minute data.
-So I filled in the NA with the mean of the column and set each of them in place of the np.nan.
-
-##### Code
+# gq_1_answer()
 
 def GQ_2():
     base_url = 'https://github.com/byuidatascience/data4missing/'
@@ -76,17 +55,7 @@ def GQ_2():
 
     return df
 
-GQ_2()
-
-#### Challenge 3
-
-We want to convert month to a numeric value in our flights data to use in our machine learning model. Please update the month column (you can use month_dict if you would like) and report the mean of the month column.
-
-##### Answer
-
-I used the Data Sictionary to make it easier for my self when I passed it through with the Replace function.
-
-##### Code
+# GQ_2()
 
 def GQ_3():
     base_url = 'https://github.com/byuidatascience/data4missing/'
@@ -106,17 +75,7 @@ def GQ_3():
     print("Median Month in the Data Set is the ", round(df.mean(), 0), "th")
     return df
 
-GQ_3()
-
-#### Challenge 4
-
-The num_of_delays_carrier column has a Dtype of object. We need to remove the + that is causing the dtype and convert it to an float64. Fix that issue in the flights data. Report the mean of num_of_delays_carrier.
-
-##### Answer
-
-Did not finish the question. Was trying to use regex to solve the problem but I had a hard time remembering what symbols to use for the Regex for the "+" sign.
-
-##### Code
+# GQ_3()
 
 def GQ_4():
     base_url = 'https://github.com/byuidatascience/data4missing/'
